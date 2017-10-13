ccd43c66362aab9be0f730c42af56127c88cf03a
Lagreze <lagrezd@gmail.com>
2017-10-13 19:02:26 +0200

    Update uptimerobot.js

--- src/uptimerobot.js
+++ src/uptimerobot.js
@@ -1,13 +1,10 @@
 //read only api keys
 export var apiKeys = [
-    //'u500085-06fb83073d443b641c44e8be', // monitoring@atypicom.fr -> Atypicom
-    //'u500085-06fb83073d443b641c44e8be', // monitoring@atypicom.fr -> Atypicom
-    'u502680-27ae3d092cce1574b1d26d74'  // monitoring1@atypicom.fr -> Satellites
+    '' // keys
 ];
 
 export function getApiRequest(key) {
-    //return 'https://api.uptimerobot.com/getMonitors?apiKey='+ key +'&responseTimes=1&responseTimesAverage=60&customUptimeRatio=1-7-30&format=json&noJsonCallback=1';
-    return 'https://api.uptimerobot.com/getMonitors?apiKey='+ key +'&responseTimes=1&responseTimesAverage=60&customUptimeRatio=1-7-30&format=json&noJsonCallback=1';
+   return 'https://api.uptimerobot.com/getMonitors?apiKey='+ key +'&responseTimes=1&responseTimesAverage=60&customUptimeRatio=1-7-30&format=json&noJsonCallback=1';
 }
 
 export function getStatusFromCode(code) {
